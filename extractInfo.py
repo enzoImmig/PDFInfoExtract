efb1e965b8f5a21b326b12c6acdc820f0177606a
Enzo Immig <enzoimmig@gmail.com>
2024-03-30 15:50:33 -0300

    feature: dinamically creates data columns according to rgxRules object

--- extractInfo.py
+++ extractInfo.py
@@ -1,12 +1,12 @@
+#Libs ---------------------------------
 import pytesseract
 from pdf2image import convert_from_path
 import re
 import pandas as pd
 
+#Constants ---------------------------------
+spreadsheet_path = 'testSpreadsheet.xlsx'
 pdf_file = 'tarefa1.pdf'
-pages = convert_from_path(pdf_file)
-dictInfo = dict(titulo="", num="", autor="", pontuacao="", entrega="", local="", instructs="")
-
 rgxRules = {
     'titulo': "(?<=TAREFA [0-9][0-9]...)(.*)",
     'num': "(?<=TAREFA )[0-9]{0,2}",
@@ -17,30 +17,43 @@ rgxRules = {
     'autor':"(?<=Boa sorte a todas as equipes! )(.*)\.",
 }
 
-infoNames = ["titulo", "num", "instructs", "pontuacao", "entrega", "local", "autor"]
+#Globals ----------------------------------
+pages = convert_from_path(pdf_file)
+dictInfo = dict()
+infoNames = []
+
+def init_Config(arrNames):
+    for name in arrNames:
+        dictInfo[name]=""
+        infoNames.append(name)
 
 def extract_text_from_image(image):
     text = pytesseract.image_to_string(image, lang="por")
     return text
 
-#extrai as informações setadas em infoNames a adiciona ela no dicionario
-for page in pages:
-    text = extract_text_from_image(page)
-    
-    #aplica os regexs
+def run():
+    #extrai as informações setadas em infoNames a adiciona ela no dicionario
+    for page in pages:
+        text = extract_text_from_image(page)
+        
+        #aplica os regexs
+        for info in infoNames:
+            dictInfo[info] = re.findall(rgxRules[info], text)[0]
+        
+    dtTasks = pd.read_excel(spreadsheet_path)
+
+    dataRow = []
     for info in infoNames:
-        dictInfo[info] = re.findall(rgxRules[info], text)[0]
-    
-dtTasks = pd.read_excel('testSpreadsheet.xlsx')
-firstFreeRowIdx = dtTasks['Tarefa'].last_valid_index()+1
+        dataRow.append(dictInfo[info])
 
-dataRow = [[dictInfo["num"], dictInfo["titulo"], dictInfo["pontuacao"], dictInfo["entrega"], dictInfo["instructs"], dictInfo["autor"],""]]
+    dtNewTask = pd.DataFrame(
+        data=[dataRow],
+        columns=infoNames,
+    )
 
-dtNewTask = pd.DataFrame(
-    data=dataRow,
-    columns=['Tarefa', 'Título', 'Pontuação', 'Lote', 'Descrição', 'Autor','Comentários'],
-    index=[firstFreeRowIdx]
-)
+    dtUpdated = pd.concat([dtTasks, dtNewTask])
+    dtUpdated.to_excel("testSpreadsheet.xlsx", index=False)
 
-dtUpdated = pd.concat([dtTasks, dtNewTask])
-dtUpdated.to_excel("testSpreadsheet.xlsx", index=False)
+if __name__ == '__main__':
+    init_Config(rgxRules.keys())
+    run()
